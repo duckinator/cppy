45c0f3a5f8143783a78e0127a1297c9ff0faf575
Ellen Marie Dash <me@duckie.co>
2018-05-16 16:56:02 -0400

    Make macros.py actually correct.

--- macros.py
+++ macros.py
@@ -1,8 +1,8 @@
 def array_literal(*values):
-    return clist(*values)
+    return clist(values)
 
 def repeat(value, length):
     return [value] * length
 
 def buffer(value, length):
-    return array_literal(repeat(char(' '), 10))
+    return array_literal(*repeat(char(value), length - 1), 0)
