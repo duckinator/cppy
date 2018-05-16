a080ef455a636a7910083a4f5d7fd035ad658782
Ellen Marie Dash <me@duckie.co>
2018-05-16 16:56:42 -0400

    *shrugs forever*

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    const char spaces[11] = buffer!(' ', 10);
-    printf("'%s'\r\n", spaces);
+    const char aaaa[11] = buffer!('A', 11);
+    printf("'%s'\r\n", aaaa);
     return 0;
 }
