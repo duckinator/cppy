e698c734a7f70adbd012b66f51d4afa26ffdd864
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-06-01 16:37:05 -0400

    preprocessor: Start adding type annotations

--- preprocessor.py
+++ preprocessor.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 import json
-from pathlib import Path
 import re
 import sys
+from pathlib import Path
+from typing import Any, Sequence
 
 class char(str):
     pass
 
-def cstr(value):
+def cstr(value: str):
     return json.dumps(value)
 
-def cchar(value):
+def cchar(value: char):
     return "'{}'".format(value[0])
 
-def cbool(value):
+def cbool(value: bool):
     return str(value).lower()
 
-def clist(value):
+def clist(value: Sequence[Any]):
     return "{" + ", ".join(map(repr, value)) + "}"
 
 crepr_functions = {
@@ -36,7 +37,7 @@ def crepr(val):
 
     return repr(fn(val))
 
-def call(fn, *args):
+def call(fn: str, *args):
     return "{}({})".format(fn, repr(clist(args))[1:-1])
 
 def import_macros(code):
